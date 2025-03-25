7d5a9a7446f9f9485db056f6c6a8737a5514faa0
Anatn <anantmann9057@gmail.com>
2025-03-25 16:05:36 +0530

    aggregation

---
+++ server/middlewares/auth.middlewares.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+import { ApiErrorResponse } from '../utils/ApiErrorResponse.js';
+import { User } from '../models/users.models.js';
+import { asyncHandler } from '../utils/asynchandler.js';
+
+export const verifyJwt = asyncHandler(async (req, res, next) => {
+    console.log(req.header("Authorization"));
+    if (!req.cookies.refreshToken && req.header('Authorization') == undefined) {
+        throw new ApiErrorResponse(401, 'User is not logged in!');
+    }
+    const token = req.cookies.refreshToken || req.header("Authorization").replace("Bearer ", "");
+    let decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+
+    if (!decodedToken) throw new ApiErrorResponse(401, 'invalid token')
+
+
+    var user = await User.findById(decodedToken._id).select('-password -refreshToken');
+
+    if (!user) throw new ApiErrorResponse(200, "user not found")
+
+    req.user = user;
+
+    next();
+});
